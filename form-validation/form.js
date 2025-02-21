adb0c746fa5da45bdb838d3deedc8599e7afec9b
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-02-21 13:11:25 +0100

    Associate an error message to each constraint

--- form-validation/form.js
+++ form-validation/form.js
@@ -4,11 +4,26 @@ const checkConstraint = (constraints, itemToCheck) => {
 }
 
 const constraints = {
-    email: ".+@.+\\.[a-z]+$",
-    country: "Italy|France|Germany",
-    postalCode: "[0-9]{5}",
-    password: "[A-Za-z0-9]{3,}",
-    passwordConfirm: ".{3,}",
+    email: {
+        rule: ".+@.+\\.[a-z]+$",
+        errorMessage: "Please enter a valid email",
+    },
+    country: {
+        rule: "Italy|France|Germany",
+        errorMessage: "The only valid Countries are Italy, France, and Germany",
+    },
+    postalCode: {
+        rule: "[0-9]{5}",
+        errorMessage: "The postal code must have 5 digits"
+    },
+    password: {
+        rule: "[A-Za-z0-9]{3,}",
+        errorMessage: "The Password must have at least 3 digits and no special characters"
+    },
+    passwordConfirm: {
+        rule: "[A-Za-z0-9]{3,}",
+        errorMessage: "The Password must have at least 3 digits and no special characters"
+    },
 }
 
 function checkCountry () {
