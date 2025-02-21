3aac27d279e59a54a0194a1993075d3d1a2ce16c
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-02-21 13:57:25 +0100

    Add functions to validate country

--- form-validation/form.js
+++ form-validation/form.js
@@ -1,6 +1,13 @@
 const checkConstraint = (constraints, itemToCheck) => {
-    const constraint = new RegExp(constraints[itemToCheck.id]);
+    const constraint = new RegExp(constraints[itemToCheck.id]["rule"]);
     return constraint.test(itemToCheck.value)
+}
+
+const showErrorMessage = (errorMessage, item) => {
+    errorContainer = document.createElement("div");
+    errorContainer.classlist.Add("errorMessage");
+    errorContainer.textContent = errorMessage;
+    item.after(errorContainer);
 }
 
 const constraints = {
@@ -26,7 +33,11 @@ const constraints = {
     },
 }
 
-function checkCountry () {
-    console.log(checkConstraint(constraints, country));
+const checkValidation = (constraints, item) => {
+    if (!checkConstraint(constraints, item)) {
+        showErrorMessage(constraints[item.id]["errorMessage"]);
+    }
 }
+
 const country = document.getElementById("country");
+country.addEventListener("blur", () => checkValidation(constraints, country))
